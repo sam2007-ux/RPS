7c661452fa861e7c584ed3abdca4dea1ebd8e4a6
Samuel Yilma <yilmasamy@gmail.com>
2023-03-13 17:42:53 -0400

    just added the play function and Playagainst Computer (not fully finished)

--- RPS.java
+++ RPS.java
@@ -1,7 +1,15 @@
 import java.lang.Thread.State;
 import java.util.Scanner;
+import java.util.function.Function;
 
+import javafx.print.Paper;
+
+import java.util.Random;
+
+//Implemtent Ignore case funcinality
 public class RPS {
+  private static final Scanner scan = new Scanner(System.in);
+
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
 
@@ -13,39 +21,110 @@ public class RPS {
     System.out.println("  - " + PLAY_OPTION + " -");
     System.out.println("  - " + HELP_OPTION + " -");
     System.out.println("  - " + LEAVE_OPTION + " -");
+
     System.out.println();
     System.out.print("Type: ");
-    String IntroOption = scan.nextLine();
+    String c = scan.nextLine();
 
-    switch (IntroOption) {
+    switch (c) {
       case PLAY_OPTION:
         clear();
+        Play();
         break;
       case HELP_OPTION:
-        // TODO: Implement help functionality
+        clear();
+        Help();
         break;
       case LEAVE_OPTION:
         System.exit(0);
         break;
       default:
-        System.out.println("Invalid option. Please try again.");
+        System.out.println("Invalid");
     }
   }
 
   public static void clear() {
     try {
-      if (System.getProperty("os.name").contains("Windows")) {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-      } else {
-        Runtime.getRuntime().exec("clear");
-      }
+      System.out.print("\033\143");
+      System.out.println(".");
+      Thread.sleep(100);
+      Thread.sleep(100);
+      Thread.sleep(100);
+      System.out.print("\033\143");
+      System.out.println("..");
+      Thread.sleep(100);
+      Thread.sleep(100);
+      Thread.sleep(100);
+      System.out.print("\033\143");
+      System.out.println("...");
+      Thread.sleep(100);
+      Thread.sleep(100);
+      Thread.sleep(100);
+      System.out.print("\033\143");
     } catch (Exception e) {
       System.out.println(e);
     }
   }
 
-  public static void help() {
+  public static void Help() {
+    try {
+      System.out.println("Here are the rules:");
+      System.out.println(" - Rock beats Scissors");
+      System.out.println(" - Scissors beats Paper");
+      System.out.println(" - Paper beats Rock");
+      System.out.println("To play, type your choice (rock, paper, or scissors) and press enter.");
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
 
+  public static void Play() {
+    try {
+      while (true) {
+        System.out.println("Do you wish to play against a Computer or a friend?");
+        System.out.print("Enter `Computer or Friend`: ");
+        String OponentChoice = scan.nextLine();
+
+        if (OponentChoice.equalsIgnoreCase("Computer")) {
+
+          // TODO : Implement PlayagainstComputer Function
+          break;
+        } else if (OponentChoice.equalsIgnoreCase("Friend")) {
+
+          // TODO : Implement PlayagainstFriend Function
+        } else {
+          System.out.println("Invalid choice, please enter 'computer' or 'friend'.");
+        }
+      }
+    } catch (Exception e) {
+      System.exit(0);
+    }
+  }
+
+  public static void PlayagainstComputer(){
+    try{
+      
+      System.out.println("your are playing against a computer");
+
+      while(true){
+        
+
+        System.out.println("Enter your choice (Rock,  Paper, or Scissors)");
+        String userChoice = scan.nextLine();
+
+      if(userChoice.equalsIgnoreCase("Rock") || userChoice.equalsIgnoreCase("Paper") || userChoice.equalsIgnoreCase("Scissors")){
+      }
+    }
   }
 
 }
+
+  
+  
+
+  
+
+  
+  
+    
+  
